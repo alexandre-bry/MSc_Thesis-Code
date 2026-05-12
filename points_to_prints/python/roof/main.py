78f484d0ecf4f2b80648329bff3af37aa09ab140
Alexandre Bry <136007010+alexandre-bry@users.noreply.github.com>
2026-05-12 14:46:25 +0200

    Improved roofer call pipeline

--- points_to_prints/python/roof/main.py
+++ points_to_prints/python/roof/main.py
@@ -6,8 +6,8 @@ import typer
 app = typer.Typer()
 
 
-@app.command("roofprints_to_roof")
-def roofprints_to_roof_command(
+@app.command("roofprints_to_lod22")
+def roofprints_to_lod22_command(
     point_cloud_path: Annotated[
         Path, typer.Argument(help="Path to the point cloud file (LAS/LAZ)")
     ],
@@ -18,14 +18,68 @@ def roofprints_to_roof_command(
         Path,
         typer.Argument(help="Path where the resulting 3D roof model will be saved"),
     ],
+    overwrite: Annotated[
+        bool,
+        typer.Option(
+            "--overwrite",
+            help="Whether to overwrite the output file if it already exists.",
+        ),
+    ] = False,
+    skip_existing: Annotated[
+        bool,
+        typer.Option(
+            "--skip_existing",
+            help="Whether to skip processing files that already exist.",
+        ),
+    ] = False,
     verbose_int: Annotated[int, typer.Option("--verbose", "-v", count=True)] = 0,
 ) -> None:
-    from .roof import roofprints_to_roof_call
+    from .roof import roofprints_to_lod22_call
 
-    roofprints_to_roof_call(
+    roofprints_to_lod22_call(
         point_cloud_path=point_cloud_path,
         roofprints_path=roofprints_path,
         roof_path=output_path,
+        overwrite=overwrite,
+        skip_existing=skip_existing,
+        verbose_int=verbose_int,
+    )
+
+
+@app.command("lod22_to_roof")
+def lod22_to_roof_command(
+    lod22_path: Annotated[
+        Path, typer.Argument(help="Path to the input LOD2.2 CityJSON file")
+    ],
+    output_path: Annotated[
+        Path,
+        typer.Argument(
+            help="Path where the resulting roof-only CityJSON file will be saved"
+        ),
+    ],
+    overwrite: Annotated[
+        bool,
+        typer.Option(
+            "--overwrite",
+            help="Whether to overwrite the output file if it already exists.",
+        ),
+    ] = False,
+    skip_existing: Annotated[
+        bool,
+        typer.Option(
+            "--skip_existing",
+            help="Whether to skip processing files that already exist.",
+        ),
+    ] = False,
+    verbose_int: Annotated[int, typer.Option("--verbose", "-v", count=True)] = 0,
+) -> None:
+    from .roof import lod22_to_roof_call
+
+    lod22_to_roof_call(
+        lod22_path=lod22_path,
+        roof_path=output_path,
+        overwrite=overwrite,
+        skip_existing=skip_existing,
         verbose_int=verbose_int,
     )
 
--- points_to_prints/python/roof/roof.py
+++ points_to_prints/python/roof/roof.py
@@ -2,74 +2,174 @@ from glob import glob
 import logging
 from pathlib import Path
 import subprocess
+from tempfile import TemporaryDirectory
 
 from ..utils.custom_logging import LoggingContext, run_command_with_tqdm_logging
 
 
-def roofprints_to_roof_implementation(
+def roofprints_to_lod22_implementation(
     point_cloud_path: Path,
     roofprints_path: Path,
     roof_path: Path,
+    overwrite: bool,
+    skip_existing: bool,
 ) -> None:
     """
     Creates a 3D roof model from roofprints and a point cloud.
     This simply calls roofer with the appropriate arguments, and then converts the CityJSONSeq output to CityJSON.
 
-    Args:
-        point_cloud_path (Path): path to the point cloud file (LAS/LAZ)
-        roofprints_path (Path): path to the roofprints file
-        roof_path (Path): CityJSON path where the resulting 3D roof model will be saved
+    Parameters
+    ----------
+    point_cloud_path : Path
+        Path to the point cloud file (LAS/LAZ).
+    roofprints_path : Path
+        Path to the roofprints file (Parquet, GeoPackage, Shapefile, ...).
+    roof_path : Path
+        Path where the resulting 3D roof model will be saved (CityJSON).
+    overwrite : bool
+        Whether to overwrite the output file if it already exists.
+    skip_existing : bool
+        Whether to skip processing if the output file already exists.
     """
 
-    # --------------------- Create the 3D building models -------------------- #
+    if roof_path.exists():
+        if skip_existing:
+            logging.info(
+                f"Output file {roof_path} already exists. Skipping processing."
+            )
+            return
+        elif not overwrite:
+            logging.error(
+                f"Output file {roof_path} already exists. Use --overwrite to overwrite it."
+            )
+            return
+        else:
+            logging.warning(f"Output file {roof_path} already exists. Overwriting it.")
 
-    roofer_output_path = roof_path.parent / f"{roof_path.stem}_roofer_output"
-    # command_roofer = [
-    #     "roofer",
-    #     str(point_cloud_path),
-    #     str(roofprints_path),
-    #     str(roofer_output_path),
-    #     "--no-clip-terrain",
-    # ]
+    with TemporaryDirectory() as temp_dir:
+        # ----------------- Convert roofprints to GeoPackage ----------------- #
+        if roofprints_path.suffix.lower() != ".gpkg":
+            logging.info(
+                f"Converting roofprints from {roofprints_path.suffix} to GeoPackage format..."
+            )
+            roofprints_gpkg_path = Path(temp_dir) / "roofprints.gpkg"
+            command_convert = [
+                "gdal",
+                "convert",
+                "-i",
+                str(roofprints_path),
+                "-o",
+                str(roofprints_gpkg_path),
+            ]
+            return_code = run_command_with_tqdm_logging(command_convert, display=True)
+            if return_code != 0:
+                logging.error(f"Failed to convert roofprints to GeoPackage format.")
+                return
+            else:
+                logging.info(f"Successfully converted roofprints to GeoPackage format.")
+        else:
+            roofprints_gpkg_path = roofprints_path
 
-    # return_code = run_command_with_tqdm_logging(command_roofer, display=True)
-    # if return_code != 0:
-    #     logging.error(f"Failed to create 3D roof model.")
-    # else:
-    #     logging.info(f"Successfully created 3D roof model.")
+        # ------------------- Create the 3D building models ------------------ #
 
-    # -------------- Convert the CityJSONSeq outputs to CityJSON ------------- #
+        roofer_output_path = Path(temp_dir) / "roofer_output"
+        command_roofer = [
+            "roofer",
+            str(point_cloud_path),
+            str(roofprints_gpkg_path),
+            str(roofer_output_path),
+            "--no-clip-terrain",
+        ]
 
-    command_cat = ["cat", *glob(str(roofer_output_path / "*.city.jsonl"))]
-    command_cjseq = [
-        "cjseq",
-        "collect",
-        ">",
-        str(roof_path),
-    ]
+        return_code = run_command_with_tqdm_logging(command_roofer, display=True)
+        if return_code != 0:
+            logging.error(f"Failed to create 3D roof model.")
+        else:
+            logging.info(f"Successfully created 3D roof model.")
 
-    ps = subprocess.Popen(command_cat, stdout=subprocess.PIPE)
-    output = subprocess.check_output(command_cjseq, stdin=ps.stdout)
-    ps.wait()
+        # ------------ Convert the CityJSONSeq outputs to CityJSON ----------- #
 
-    # return_code = run_command_with_tqdm_logging(command, display=True)
-    # if return_code != 0:
-    #     logging.error(f"Failed to convert CityJSONSeq to CityJSON.")
-    # else:
-    #     logging.info(f"Successfully converted CityJSONSeq to CityJSON.")
+        command_cat = ["cat", *glob(str(roofer_output_path / "*.city.jsonl"))]
+        command_cjseq = ["cjseq", "collect"]
+
+        logging.info(
+            f"Running command: {' '.join(command_cat)} | {' '.join(command_cjseq)} > {roof_path}"
+        )
+        with roof_path.open("wb") as roof_file:
+            ps = subprocess.Popen(command_cat, stdout=subprocess.PIPE)
+            try:
+                result = subprocess.run(
+                    command_cjseq, stdin=ps.stdout, stdout=roof_file
+                )
+                return_code = result.returncode
+            finally:
+                if ps.stdout is not None:
+                    ps.stdout.close()
+                ps.wait()
+
+        if return_code != 0:
+            logging.error("Failed to convert CityJSONSeq to CityJSON.")
+        else:
+            logging.info("Successfully converted CityJSONSeq to CityJSON.")
 
 
-def roofprints_to_roof_call(
+def roofprints_to_lod22_call(
     point_cloud_path: Path,
     roofprints_path: Path,
     roof_path: Path,
+    overwrite: bool,
+    skip_existing: bool,
     verbose_int: int = 0,
 ) -> None:
     with LoggingContext(verbose=verbose_int):
         roof_path.parent.mkdir(parents=True, exist_ok=True)
 
-        roofprints_to_roof_implementation(
+        roofprints_to_lod22_implementation(
             point_cloud_path=point_cloud_path,
             roofprints_path=roofprints_path,
             roof_path=roof_path,
+            overwrite=overwrite,
+            skip_existing=skip_existing,
+        )
+
+
+def lod22_to_roof_implementation(
+    lod22_path: Path,
+    roof_path: Path,
+    overwrite: bool,
+    skip_existing: bool,
+) -> None:
+    """
+    Extracts the roof from a LOD2.2 CityJSON file and saves it as a separate CityJSON file.
+
+    Parameters
+    ----------
+    lod22_path : Path
+        Path to the input LOD2.2 CityJSON file.
+    roof_path : Path
+        Path where the resulting roof-only CityJSON file will be saved.
+    overwrite : bool
+        Whether to overwrite the output file if it already exists.
+    skip_existing : bool
+        Whether to skip processing if the output file already exists.
+    """
+
+    raise NotImplementedError("This function is not yet implemented.")
+
+
+def lod22_to_roof_call(
+    lod22_path: Path,
+    roof_path: Path,
+    overwrite: bool,
+    skip_existing: bool,
+    verbose_int: int = 0,
+) -> None:
+    with LoggingContext(verbose=verbose_int):
+        roof_path.parent.mkdir(parents=True, exist_ok=True)
+
+        lod22_to_roof_implementation(
+            lod22_path=lod22_path,
+            roof_path=roof_path,
+            overwrite=overwrite,
+            skip_existing=skip_existing,
         )
